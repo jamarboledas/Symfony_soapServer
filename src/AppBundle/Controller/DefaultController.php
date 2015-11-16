68ecd0d5dd0b734af969a1642c7a518ecd99b16c
jamarboledas <i02maarj@uco.es>
2015-11-16 10:14:04 +0100

    soap server has been created

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,18 +4,39 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class DefaultController
+ * @package AppBundle\Controller
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/server", name="server")
      */
-    public function indexAction(Request $request)
+    public function serverAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        $server = new \SoapServer('web/public/hello.wsdl');
+        $server->setObject($this->get('hello_service'));
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'text/xml; charset=ISO-8859-1');
+
+        ob_start();
+        $server->handle();
+        $response->setContent(ob_get_clean());
+
+        return $response;
+    }
+
+    /**
+     * @Route("/client", name="client")
+     */
+    public function clientAction()
+    {
+        $client = new \SoapClient('/server?wsdl', true);
+
+        $result = $client->call('hello', array('name' => 'Scott'));
     }
 }
